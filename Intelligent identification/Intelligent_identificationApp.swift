810ee1f4a21e1619d8c9a6d455118cde3fdd8fc5
4 <1>
2025-10-24 19:31:46 +0800

    Initial Commit

---
+++ Intelligent identification/Intelligent_identificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Intelligent_identificationApp.swift
+//  Intelligent identification
+//
+//  Created by Jiahong Chen  on 10/24/25.
+//
+
+import SwiftUI
+
+@main
+struct Intelligent_identificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Intelligent identificationTests/Intelligent_identificationTests.swift
@@ -0,0 +1,17 @@
+//
+//  Intelligent_identificationTests.swift
+//  Intelligent identificationTests
+//
+//  Created by Jiahong Chen  on 10/24/25.
+//
+
+import Testing
+@testable import Intelligent_identification
+
+struct Intelligent_identificationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
